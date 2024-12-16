7cd3d7f8abcf998d4845b308ecdf67025375803c
Khawla Ben hammou <benhammoukhawla99@gmail.com>
2024-12-16 18:08:22 +0100

    new updates

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,8 @@
 function displayMessage(message) {
-  console.log(message);
+  console.log(`${message}`);
 }
 
 module.exports = displayMessage;
+console.log('Application started.');
+console.log('Application running...');
+console.log('Application stopped.');
